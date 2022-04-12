a22485372c89c076c80cf7714d980b6948fdc68e
Laxman1500 <94347196+Laxman1500@users.noreply.github.com>
2022-04-12 15:05:40 +0530

    Update provison.tf

--- provison.tf
+++ provison.tf
@@ -1,18 +1,16 @@
-resource "aws_iam_role" "eks_cluster" {
- name = "eks-cluster"
- assume_role_policy = <<POLICY
- {
- "Version": "2012-10-17",
- "Statement": [
- {
- "Effect": "Allow",
- "Principal": {
- "Service": "eks.amazonaws.com"
- },
- "Action": "sts:AssumeRole"
+module "vpc" {
+ source = "terraform-aws-modules/vpc/aws"
+name = "my-vpc"
+ cidr = "10.0.0.0/16"
+azs = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+ public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+enable_nat_gateway = true
+enable_vpn_gateway = true
+ tags = {
+ Terraform = "true"
+ Environment = "dev"
  }
- ]
- }
- POLICY
- }
+}
+
 
