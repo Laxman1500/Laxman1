10603bd72a4408c0ab0ff5e053b51dee87c008a5
Laxman1500 <94347196+Laxman1500@users.noreply.github.com>
2022-04-12 13:14:15 +0530

    Update provison.tf

--- provison.tf
+++ provison.tf
@@ -1,34 +1,11 @@
-Provison "aws_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-
-  name = "single-instance"
-
-  ami                    = "ami-ebd02392"
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+provider "aws" {
+  profile    = "default"
+  region     = "us-east-1"
 }
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
- name = "my-vpc"
-  cidr = "10.0.0.0/16"
- azs             = ["eu-west-1a"]
-  private_subnets = ["10.0.1.0/24"]
-  public_subnets  = ["10.0.101.0/24"]
- enable_nat_gateway = true
-  enable_vpn_gateway = true
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
+
+resource "aws_instance" "example" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
 }
-  # code from terraform 
+
   
