04866a8ad224705690aebb2a2671cc49a911c238
Laxman1500 <94347196+Laxman1500@users.noreply.github.com>
2022-04-13 10:28:51 +0530

    Update ec2_Terraform.tf

--- ec2_Terraform.tf
+++ ec2_Terraform.tf
@@ -1,9 +1,9 @@
-provider "aws_instance" {
-  profile    = "Ubuntu"
+provider "aws" {
+  profile    = "default"
   region     = "us-east-1"
 }
 
-resource "aws_instance" "terraform_ec2" {
+resource "aws_instance" "instance" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
 }
