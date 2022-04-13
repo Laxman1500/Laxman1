3d6eaca32fd620fc04d941e0e3fa437e43ce87cb
Laxman1500 <94347196+Laxman1500@users.noreply.github.com>
2022-04-13 10:43:48 +0530

    Update ec2_Terraform.tf

--- ec2_Terraform.tf
+++ ec2_Terraform.tf
@@ -1,7 +1,4 @@
-provider "aws" {
-  region = "us-east-2"
-}
-resource "aws_instance" "myfirstinstance"{
+resource "aws_instance" "MyFirstInstance" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
   tags{
